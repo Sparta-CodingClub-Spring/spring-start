5484eb118ae1fc3f659dc4e6f89fea2aaac653f2
ckwogus37 <ckwogus37@gmail.com>
2025-01-18 13:08:25 +0900

    데이터를 Client에게 반환하는 방법

---
+++ src/main/java/sparta/spring_start/springmvc/response/Star.java
@@ -0,0 +1,12 @@
+package sparta.spring_start.springmvc.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Star {
+	private String name;
+	private int age;
+
+}
